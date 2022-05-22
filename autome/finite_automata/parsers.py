62078784a785a0699254dad24c9bd02a88e6edf7
João Vitor Maia <maia.tostring@gmail.com>
2022-05-22 19:46:50 -0300

    Adiciona método para salvar máquinas em arquivos .jff

--- autome/finite_automata/parsers.py
+++ autome/finite_automata/parsers.py
@@ -1,5 +1,12 @@
 import json
-from autome.finite_automata import State, Transition, DeterministicFiniteAutomata, NonDeterministicFiniteAutomata
+
+from autome.finite_automata import (
+    State,
+    Transition,
+    DeterministicFiniteAutomata,
+    NonDeterministicFiniteAutomata,
+    transition,
+)
 from pathlib import Path
 from xml.etree import ElementTree as ET
 from typing import Callable, List, Union
@@ -7,27 +14,63 @@ from typing import Callable, List, Union
 
 class JFlapConverter:
     @classmethod
-    def parse(cls, source: Path, deterministic=True) -> Union[NonDeterministicFiniteAutomata, DeterministicFiniteAutomata]:
+    def parse(
+        cls, source: Path, deterministic=True
+    ) -> Union[NonDeterministicFiniteAutomata, DeterministicFiniteAutomata]:
         """
         Converts a .jff file into a Finite State Automata. Some serious shit may happen if you manually edit the .jff file, don't do it.
         """
         tree = ET.parse(source)
         root = tree.getroot()
         automaton: ET.Element = root.find("./automaton")
-        
+
         state_models: List[ET.Element] = automaton.findall("./state")
         states: List[State] = list(map(JFlapConverter.__parse_state__, state_models))
-        
+
         transition_models: List[ET.Element] = automaton.findall("./transition")
-        
+
         transitions = []
         for model in transition_models:
             transitions.append(JFlapConverter.__parse_transition__(model, states))
-            
+
         if deterministic:
             return DeterministicFiniteAutomata(states=states, transitions=transitions)
         else:
-            return NonDeterministicFiniteAutomata(states=states, transitions=transitions)
+            return NonDeterministicFiniteAutomata(
+                states=states, transitions=transitions
+            )
+
+    @classmethod
+    def save(cls, source: DeterministicFiniteAutomata, output_path: Path) -> bool:
+        root = ET.Element("structure")
+        type = ET.SubElement(root, "type")
+        type.text = "fa"
+        automaton = ET.SubElement(root, "automaton")
+
+        for index, state in enumerate(source.states):
+            _state = ET.SubElement(automaton, "state")
+            _state.set("name", str(state.name))
+            _state.set("id", str(state.id))
+            ET.SubElement(_state, "x").text = f"{index * 30}"
+            ET.SubElement(_state, "y").text = "50"
+
+            if state.accept:
+                ET.SubElement(_state, "final")
+
+            if state.initial:
+                ET.SubElement(_state, "initial")
+
+        for index, transition in enumerate(source.transitions):
+            _transition = ET.SubElement(automaton, "transition")
+            ET.SubElement(_transition, "from").text = str(transition.origin.id)
+            ET.SubElement(_transition, "to").text = str(transition.destiny.id)
+            ET.SubElement(_transition, "read").text = transition.symbol
+
+        document = ET.ElementTree(root)
+
+        with open(output_path, "wb") as file:
+            document.write(file, encoding="utf8", xml_declaration=True)
+            return True
 
     @classmethod
     def __parse_state__(cls, model: ET.Element) -> State:
@@ -44,20 +87,22 @@ class JFlapConverter:
         origin = next(filter(lambda state: state.name == origin_id, states))
         destiny = next(filter(lambda state: state.name == destiny_id, states))
         reads = model.findall("./read")[0].text
-        
+
         return Transition(origin, destiny, reads)
 
 
 class JSONConverter:
     @classmethod
-    def parse(cls, source: Path, deterministic=True) -> Union[NonDeterministicFiniteAutomata, DeterministicFiniteAutomata]:
+    def parse(
+        cls, source: Path, deterministic=True
+    ) -> Union[NonDeterministicFiniteAutomata, DeterministicFiniteAutomata]:
         """
         Creates an instance of Machine parsed from a json file located at @path that follows the schematics provided by the project (see /machines folder)
 
         Throws FileNotFoundError if there's no file at @path
         Throws ValueError if the json file doesn't match the schematics
         """
-        
+
         with open(source, "r", encoding="utf8") as file:
             model = json.loads(file.read())
             states = [State.parse(state) for state in model["states"]]
@@ -69,4 +114,6 @@ class JSONConverter:
         if deterministic:
             return DeterministicFiniteAutomata(states=states, transitions=transitions)
         else:
-            return NonDeterministicFiniteAutomata(states=states, transitions=transitions)
+            return NonDeterministicFiniteAutomata(
+                states=states, transitions=transitions
+            )
